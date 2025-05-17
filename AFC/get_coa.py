f9486b518d6121c6115951ec6d24a13c01abcc70
rukus05 <zkasilag@gmail.com>
2025-05-17 02:32:45 -0700

    Created AFC Payroll File.  v1

--- AFC/get_coa.py
+++ AFC/get_coa.py
@@ -15,7 +15,7 @@ def getCOA(df):
     
     # converts the DataFrame into a dictionary where the keys are the index values  
     # and the values are dictionaries containing the column names and corresponding values for each row.
-    df = df.drop(columns=['Description', 'DR/CR'])
+    #df = df.drop(columns=['Description', 'DR/CR'])
     ed = df.set_index("Payroll Item").to_dict("index")
 
     # iterate over each item (key-value pair) in the dictionary 
@@ -23,6 +23,23 @@ def getCOA(df):
     for k1, v1 in ed.items(): 
         # Use list comprehension to remove decimal points from the dictionary
         # Change k1 : v1 key value pair to k : v (where v is v1 converted to an integet)
-        ed[k1] = {k: int(v) for k, v in v1.items()}
+        ed[k1] = {k: v for k, v in v1.items()}
+       
+    return ed
+
+def get_dept_to_location(df):
+   
+    
+    # converts the DataFrame into a dictionary where the keys are the index values  
+    # and the values are dictionaries containing the column names and corresponding values for each row.
+    #df = df.drop(columns=['Description', 'DR/CR'])
+    ed = df.set_index("Primary Job Title").to_dict("index")
+
+    # iterate over each item (key-value pair) in the dictionary 
+    #print(df.head)
+    for k1, v1 in ed.items(): 
+        # Use list comprehension to remove decimal points from the dictionary
+        # Change k1 : v1 key value pair to k : v (where v is v1 converted to an integet)
+        ed[k1] = {k: v for k, v in v1.items()}
        
     return ed
--- AFC/payroll.py
+++ AFC/payroll.py
@@ -4,18 +4,15 @@ import numpy as np
 import re
 import tkinter as tk
 from tkinter import filedialog as fd
-# from definitions import coa_dict as coa
-# from afc_module.definitions import coa_dict as coa
+#from definitions import coa_dict as coa
+#from afc_module.definitions import coa_dict as coa
 from afc_module.definitions import hdc_list as hdcl
 from afc_module.definitions import roll_up_accts as rollup
 from afc_module.definitions import remove_acct_list as remove_accts
-from afc_module.definitions import credit_acct_list as cr_accts
-from afc_module.definitions import credit_rollup_accts as credit_rollups
-from afc_module.definitions import locations_dict as ld
-from afc_module.definitions import duplicate_debits_dict as duplicate_debits
 from zpack.fns import FilePrompt
 from zpack.fns import save_dataframe
 from get_coa import getCOA
+from get_coa import get_dept_to_location
 #from definitions import baseline_accounts as baseline
 
 
@@ -30,171 +27,120 @@ def main():
     df_COA_file = df_COA_file.reset_index()
     df_COA_file.fillna(0, inplace=True)
 
-    df_COA = getCOA(df_COA_file)
-    print(df_COA)
-    #  To see how dictionary looks, see the COA_Dict.py file in the archive
+    dict_COA = getCOA(df_COA_file)
+    #print(dict_COA)
+    #  To see how dictionary looks, see the COA_Dict.py file in the Dict Directory
+    # Create a list for Debit and Credit Accounts.
+    debit_accounts = []
+    credit_accounts = []
+    for k, v in dict_COA.items():
+        #print(k)
+        #print(v)
+        if v['DR/CR'] == 'Debit':
+            debit_accounts.append(k)
+        elif v['DR/CR'] == 'Credit':
+            credit_accounts.append(k)
+        
+    #print('The debit accounts are:', debit_accounts)
+    #print('')
+    #print('The credit accounts are', credit_accounts)
+    
 
-    # If the raw data file has all the data in one file (002, 007, 008 sheets), you must save each entity in it's own file and run the program against each.
+    # Get the Dept Location lookup File
+    print('Select the latest Dept-to-Location lookup file:')
+    z = FilePrompt()
+    df_d2l_file = pd.read_excel(z)
+    df_d2l_file = df_d2l_file.reset_index()
+    df_d2l_file.fillna(0, inplace=True)
+
+    dict_d2l = get_dept_to_location(df_d2l_file)
+    #print(dict_d2l)
+    
+
+    
+    
     # Read in Data from the "RawData.xlsx" file.
     print('Select the raw data file to run Payroll for:')
     f = FilePrompt()
-    df_toi = pd.read_excel(f)
-    #df_toi = pd.read_excel(f, dtype={'Home Department Code': str})
+    df_afc = pd.read_excel(f)
+    #df_afc = pd.read_excel(f, dtype={'Home Department Code': str})
 
-    df_toi = df_toi.reset_index()
+    df_afc = df_afc.reset_index()
     # Fill all blank cells with zeros.  
     # It's critiacal that any columns you do calculations do not have blanks.
-    # df_toi.fillna(0, inplace=True)
-    # Put column headers into a List
-    all_col_headers = list(df_toi.columns)   
+    #df_afc.fillna(0, inplace=True)
+    
+    # Add to the raw data Dataframe 
+    df_afc['Department ID'] = ''
+    df_afc['LOCATION'] = ''
+    df_afc['Class'] = ''
+    
+    # Put column headers into a Lis
+    all_col_headers = list(df_afc.columns)
+    #print(all_col_headers)
+    #print('')   
 
-    # Remove the columns not used in the JE's
-    # result_list = [x for x in original_list if x not in elements_to_remove]
-    col_headers = [x for x in all_col_headers if x not in remove_accts]
-   
-    no_of_columns = len(col_headers)    
-    
-    # Remember:  Index is at 0, then first column, second column, etc, etc.  To get last Column, subtract 1
-    # Like this:  print(col_headers[no_of_columns - 1])  
-    
-    # Reduce the size of the col_headers list to start with the Columns we want.  In this case, "Regular Earnings Total"
-    RET = "Regular Earnings Total"
-    if RET in col_headers:
-        # Find index for element of "Regular Earnings Total"
-        index_ret = col_headers.index(RET)
-
-    # Create a new list that starts with the "Regular Earnings Total" header.
-    money_headers = col_headers[index_ret:]
-    last_header = 'Batch Number'
+    # Create a list based on the keys of the COA dict.  These are the columns used for Journal Entries
+    JE_list = list(dict_COA)
     
     
-    # Remove the last columns that aren't necessary.
-    while last_header == 'Batch Number' or last_header == 'Position ID':
-        money_headers.pop()
-        lh = money_headers[-1]
-        last_header = lh
-    
-    #print(last_header)
-    # Save the number of money headers.
-    size_of_money_headers = len(money_headers)
-    #print(size_of_money_headers)
-    '''
-    # Not used anymore#
-    if size_of_money_headers != 199:
-        print('Columns in Raw Data File have changed!!!!')
-    else:
-        print('Columns in Raw Data File appear to be unchanged')
-    '''
+    #df_afc['Credit Sum'] = df_afc[credit_accounts].sum(axis=1)
+    #df_afc['Debit Sum'] = df_afc[debit_accounts].sum(axis=1)
 
     # Convert the Pay Date column to a datetime data type
-    df_toi['Pay Date'] = pd.to_datetime(df_toi['Pay Date'])
+    df_afc['Payroll pay date'] = pd.to_datetime(df_afc['Payroll pay date'])
     # Remove the hours, minutes, and seconds
-    df_toi['Pay Date'] = df_toi['Pay Date'].dt.date
-    print('This input file is for Company ' + df_toi['Company Code'].iloc[0])
+    df_afc['Payroll pay date'] = df_afc['Payroll pay date'].dt.date
+    
+    #Verify company you're running this program for by printing out the company name
+    #print('This input file is for Company ' + df_afc['Company Code'].iloc[0])
+
+    for index, row in df_afc.iterrows():
+        df_afc.loc[index, 'Department ID'] = dict_d2l[df_afc.loc[index, 'Primary job title']]['Department ID']
+        df_afc.loc[index, 'LOCATION'] = dict_d2l[df_afc.loc[index, 'Primary job title']]['LOC']
+        df_afc.loc[index, 'Class'] = dict_d2l[df_afc.loc[index, 'Primary job title']]['Class']
+        #print (df_afc.loc[index, 'Department ID'])
+    '''
+    print("Test the DF_AFC till here")
+    # Start the "Save As" dialog box.
+    app = tk.Tk()
+    app.title("Save File As")
+    status_label = tk.Label(app, text="", fg="green")
+    status_label.pack()
+    save_button = tk.Button(app, text="Save as", command=save_dataframe(df_afc, status_label))
+    save_button.pack(padx=20, pady=10)
+    '''
+       
+    # Create new Dataframe for the Output.
+    df_Output = pd.DataFrame(columns=['DEPARTMENT', 'LOCATION',  'Class', 'Pay Date', 'Pay Period', 'GL Account', 'Dr', 'CR', 'Description'])
+    
 
     # Group the Data Frame by Company Code,  Home Department Code, and Location.
-    df_groupby = df_toi.groupby(['Batch Number', 'Home Department Code', 'Location Description', 'Pay Date'])
-    
-    
-    # Create a list the size of the columns of interest, "money_headers"
-    values_list = [0 for _ in range(size_of_money_headers)]
-    #print(values_list)
-    #print(size_of_money_headers)
-    
-    # Create new Dataframe for the Output.
-    df_Output = pd.DataFrame(columns=['Pay Date', 'Account Number', 'Description', 'Debit Amount', 'Credit Amount', 'Location', 'Dept'])
-    
+    df_groupby = df_afc.groupby(['Department ID', 'LOCATION', 'Class', 'Payroll pay date'])
+    #Verify the groupby columns
+    #print(df_afc.columns)
+
     for groupings, row in df_groupby:
-        # Find out what Home Department Code this row is, and get the index from the Home Department Code List
-        if groupings[1] in hdcl:
-            #hdc_index = hdcl.index(groupings[1])
-            homedeptcode = groupings[1]
-        else:
-           print(f"{groupings[1]} is not in the Home Dept Codes!")
-        # Use the Home Department Code Index to get the right GL's from the Chart of Accounts
-
-        # Dictionary Defining the Roll up Accounts, and initialize sum amd G/L for each to 0.  
-        rollupsums = {
-            "Wages" : [0, 0], "401K Payable" : [0, 0], "Bonus-B_Bonus" : [0, 0], "FICA" : [0, 0], "Garnishments" : [0, 0], "HSA_Deduction" : [0, 0], "Medical Ins Ded" : [0, 0], \
-            "Net Pay" : [0, 0], "Other Payroll Taxes" : [0, 0], "Overtime" : [0, 0], "Physician Bonus" : [0, 0], "Severance Expense" : [0, 0], "Tax Deduction" : [0, 0]
-        }
-        # Vaulues list counter
-        counter = 0
-        for i in money_headers:
-            found = False
-            lookupkey = ""
-            for key, value_list in rollup.items():
-                if i in value_list:
-                    found = True
-                    lookupkey = key
-                    break
-            # If this payroll item was found above, and the COA is not empty for it, then execute.      
+        #print(groupings)
+        #print(row)
+        # Create a dictionary to hold the sums for the Journal Entry items (initialized to 0) and G/L Accounts (initialized to 0)
+        JE_dict = {key: [0, 0] for key in JE_list}
+    
+        for i in JE_list:
+            account = ''
+            if i in debit_accounts:
+                account = "Debit"
+                JE_dict[i][0] += row[i].sum()
+                JE_dict[i][1] = dict_COA[i][groupings[0]]
+                df_Output.loc[len(df_Output.index)] = [groupings[0], groupings[1], groupings[2], groupings[3], row['Payroll'], JE_dict[i][1], JE_dict[i][0], '', i]
+            elif i in credit_accounts:
+                account = "Credit"
+                JE_dict[i][0] += row[i].sum()
+                JE_dict[i][1] = dict_COA[i][groupings[0]]
+                df_Output.loc[len(df_Output.index)] = [groupings[0], groupings[1], groupings[2], groupings[3], row['Payroll'], JE_dict[i][1], '', JE_dict[i][0], i]
             
-            # if found and (i in coa and coa[i]):
-            if found and (i in df_COA and df_COA[i][homedeptcode] != 0):
-                rollupsums[lookupkey][0] += row[i].sum()
-                rollupsums[lookupkey][1] = df_COA[i][homedeptcode]
-                
-            else:    
-                if i == 'PHA_Phone Allowance_Deduction':
-                    values_list[counter] = abs(row[i].sum())
-                else:
-                    values_list[counter] = row[i].sum()
-                if values_list[counter] != 0:
-                    if df_COA[i][homedeptcode]:
-                       
-                        # Convert data type to datetime64[ns]
-                        ped = groupings[3]
-                                                
-                        # Clean up Batch Number text
-                        '''
-                        text = str(row['Batch Number'])
-                        keyword = "Name"
-                        parts = text.split(keyword)
-                        if len(parts) > 1:
-                            truncated_text = parts[0]
-                        else:
-                            truncated_text = text
-                        '''
 
-                                                
-                        # If matches for credit accounts, else it's a debit account.  Values are printed in appropriate column. 
-                        # To address leading or trailing spaces in Location Descriptions, strip() method had to be employed whereever groupings[2] of the groupby object was referenced.
-                        if i in cr_accts:
-                            df_Output.loc[len(df_Output.index)] = [ped, df_COA[i][homedeptcode], str(groupings[0]) + ' ' + ' ' + str([i]), "", values_list[counter], ld[groupings[2].strip()], homedeptcode]
-                        else:
-                            if i in duplicate_debits:
-                                df_Output.loc[len(df_Output.index)] = [ped, df_COA[i][homedeptcode], str(groupings[0]) + ' ' + str([i]), values_list[counter], "", ld[groupings[2].strip()], homedeptcode]
-                                df_Output.loc[len(df_Output.index)] = [ped, '14900', str(groupings[0]) + ' ' + str([i]), "", values_list[counter], ld[groupings[2].strip()], homedeptcode]
-                            else: 
-                                df_Output.loc[len(df_Output.index)] = [ped, df_COA[i][homedeptcode], str(groupings[0]) + ' ' + str([i]), values_list[counter], "", ld[groupings[2].strip()], homedeptcode]
-            counter += 1
-        #print(rollupsums)
-        for acct, z in rollupsums.items():
-            if z[0] != 0:
-                ped = groupings[3]
-                
-                # Clean up Batch Number text
-                '''
-                text = str(row['Batch Number'])
-                keyword = "Name"
-                parts = text.split(keyword)
-                if len(parts) > 1:
-                    truncated_text = parts[0]
-                else:
-                    truncated_text = text
-                '''
-
-                # If matches for credit accounts, else it's a debit account
-                if acct in credit_rollups:
-                    df_Output.loc[len(df_Output.index)] = [ped, z[1], str(groupings[0]) + ' ' + acct, "", z[0], ld[groupings[2].strip()], homedeptcode]
-                else:
-                    if acct in duplicate_debits:
-                        df_Output.loc[len(df_Output.index)] = [ped, z[1], str(groupings[0]) + ' ' + acct, z[0], "", ld[groupings[2].strip()], homedeptcode]
-                        df_Output.loc[len(df_Output.index)] = [ped, '14900', str(groupings[0]) + ' ' + acct, "", z[0], ld[groupings[2].strip()], homedeptcode]
-                    else:
-                        df_Output.loc[len(df_Output.index)] = [ped, z[1], str(groupings[0]) + ' ' + acct, z[0], "", ld[groupings[2].strip()], homedeptcode]
-                        
+        
     runningtime = time.time() - start
     print("Save the Output File...")
     # Start the "Save As" dialog box.
